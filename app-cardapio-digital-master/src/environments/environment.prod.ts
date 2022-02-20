cfed74523e19d0e61ef18bfe790b7b076f92d5a3
climaco <climacobnu@gmail.com>
2022-02-20 00:04:22 -0300

    ok

---
+++ app-cardapio-digital-master/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBnyd_sWrV7siovcz9_QKRiFBAFBmDraYQ",
+    authDomain: "app--cardapio-digital.firebaseapp.com",
+    projectId: "app--cardapio-digital",
+    storageBucket: "app--cardapio-digital.appspot.com",
+    messagingSenderId: "706808880303",
+    appId: "1:706808880303:web:7eded7dc73a962443b085a",
+    measurementId: "G-P3Q8SRRT10"
+  }
+ };
